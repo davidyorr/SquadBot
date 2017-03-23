4cddc5d65dad0056ba2b328565345363f81add6f
David Orr <davidyorr@gmail.com>
2017-03-22 22:20:52 -0400

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ const bot = new Discord.Client();
 const Auth = require('../auth.json');
 const token = Auth.token;
 
-parseMessage = (message) => {
+const parseMessage = (message) => {
   if (message.author.id === bot.user.id) {
     return;
   }
 
   if (message.isMentioned(bot.user)) {
-    messageSplit = message.content.split(' ');
+    let messageSplit = message.content.split(' ');
     if (messageSplit.length > 1) {
-      command = messageSplit[1];
+      let command = messageSplit[1];
       executeCommand(command, message);
     }
   }
 }
 
-executeCommand = (command, message) => {
+const executeCommand = (command, message) => {
   switch (command) {
     case 'ping': {
       message.channel.sendMessage('pong');
