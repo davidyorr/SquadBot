060289c3a081dc455c54e00453fe10889d62adee
David Orr <davidyorr@gmail.com>
2020-09-04 22:35:38 -0400

    Add prettier

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import { Client } from "discord.js";
 
 const bot = new Client();
 
-const token = DEV ? require('../token.dev.js') : require('../token.js');
+const token = DEV ? require("../token.dev.js") : require("../token.js");
 
-const SquadBot = require('./squadbot.js')(bot);
+const SquadBot = require("./squadbot.js")(bot);
 
-bot.on('ready', () => {
-  console.log('I am ready!');
+bot.on("ready", () => {
+  console.log("I am ready!");
 });
 
-bot.on('message', (message) => {
+bot.on("message", (message) => {
   SquadBot.parseMessage(message);
 });
 
--- src/squadbot.ts
+++ src/squadbot.ts
@@ -1,13 +1,13 @@
 import { Client, Message } from "discord.js";
 
 const Commands = {
-  'ping': {
-    description: 'responds with pong',
+  ping: {
+    description: "responds with pong",
     execute: (message: Message) => {
-      message.channel.send('pong');
-    }
-  }
-}
+      message.channel.send("pong");
+    },
+  },
+};
 
 const parseMessage = (message: Message) => {
   if (message.author.id === bot.user.id) {
@@ -15,27 +15,27 @@ const parseMessage = (message: Message) => {
   }
 
   if (message.mentions.has(bot.user)) {
-    let messageSplit = message.content.split(' ');
+    let messageSplit = message.content.split(" ");
     if (messageSplit.length > 1) {
       let command = messageSplit[1] as keyof typeof Commands;
       executeCommand(command, message);
     }
   }
-}
+};
 
 const executeCommand = (command: keyof typeof Commands, message: Message) => {
   if (Commands[command]) {
     Commands[command].execute(message);
   } else {
-    message.channel.send('sorry');
+    message.channel.send("sorry");
   }
-}
+};
 
 const SquadBot = {
   Commands,
   parseMessage,
-  executeCommand
-}
+  executeCommand,
+};
 
 var bot: any;
 
